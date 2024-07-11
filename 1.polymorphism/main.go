b344b1a4a76cd0b7fa542feed580982e7f362c9d
kino <kino@pop-os.localdomain>
2024-07-12 01:38:54 +0200

    snippet

---
+++ 1.polymorphism/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type greeting interface {
+	hey()
+}
+
+type Person struct {
+	Name string
+	Age int
+}
+
+type Dog struct {
+	Name string
+	Breed string
+}
+
+func (p *Person) hey(){
+	fmt.Println("Hello")
+}
+
+func (d *Dog) hey(){
+	fmt.Println("Ooof oof")
+}
+
+func sayHey(welcomer greeting) {
+	welcomer.hey()
+}
+
+func main() {
+	var ayoub *Person
+	var scobido Dog
+
+	ayoub.hey()
+	scobido.hey()
+	
+	// using Polymorphism
+	sayHey(ayoub)
+	sayHey(&scobido)
+
+}
